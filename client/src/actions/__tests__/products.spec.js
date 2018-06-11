6a1005bc318463170edef62d7af57eb9b23ec272
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2018-06-11 01:09:40 -0300

    tests: action tests

---
+++ client/src/actions/__tests__/products.spec.js
@@ -0,0 +1,40 @@
+import {
+  requestProductsFail,
+  requestProducts,
+  requestProductsSuccess
+} from '../products'
+
+const products = [{}, {}, {}]
+
+const err =  new TypeError('ERROR');
+
+describe('#products', () => {
+  describe('#actions', () => {
+    it('should create an action to fetch products', () => {
+      const expectedAction = {
+        type: 'PRODUCTS_REQUEST_FETCH'
+      }
+
+      expect(requestProducts()).toEqual(expectedAction)
+    })
+
+    it('should create an action to fetch products success', () => {
+      const expectedAction = {
+        type: 'PRODUCTS_SUCCESS_FETCH',
+        payload: { products }
+      }
+
+      expect(requestProductsSuccess({ products })).toEqual(expectedAction)
+    })
+
+    it('should create an action to fetch products fail', () => {
+      const expectedAction = {
+        type: 'PRODUCTS_FAIL_FETCH',
+        payload: err,
+        error: true
+      }
+
+      expect(requestProductsFail(err)).toEqual(expectedAction)
+    })
+  })
+})
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,7 +1,5 @@
 import Adapter from 'enzyme-adapter-react-16'
 import { configure } from 'enzyme'
 import 'jest-enzyme'
-import 'jest-localstorage-mock'
-import 'jest-styled-components'
 
 configure({ adapter: new Adapter() })
